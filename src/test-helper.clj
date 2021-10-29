dec5b968e662e8871a8e1379d8e52aa06210f3c9
solar05 <artem00298@gmail.com>
2021-10-30 02:08:11 +0300

    Add one more test helper

--- src/test-helper.clj
+++ src/test-helper.clj
@@ -7,3 +7,10 @@
     (let [out (s/trim (with-out-str (load-file "index.clj")))]
       (is (= out expected))))
   (when-not (successful? (run-tests 'test-helper)) (System/exit 1)))
+
+(defn ^:export assert-solution [samples expected solution]
+  (let [combined (map vector samples expected)]
+    (deftest solution-test
+      (doseq [[sample result] combined]
+        (is (= (solution sample) result)))))
+  (when-not (successful? (run-tests 'test-helper)) (System/exit 1)))
