cc1c908436a9f6d0f02132dacfb78034adb738e2
solar05 <artem00298@gmail.com>
2021-10-30 23:02:55 +0300

    Fix lint

--- modules/30-logic/50-expressions/index.clj
+++ modules/30-logic/50-expressions/index.clj
@@ -3,9 +3,9 @@
 (defn do-today [day-of-week]
   (cond
     (and (int? day-of-week)
-          (<= 1 day-of-week 7))
-     (case day-of-week
-       (6 7) "rest"
-       "work")
+         (<= 1 day-of-week 7))
+    (case day-of-week
+      (6 7) "rest"
+      "work")
     :else "???"))
 ;END
