819e5bec6cafe926c6b90f6c9e5f0b274956fd4e
solar05 <artem00298@gmail.com>
2021-10-31 02:27:02 +0300

    Add fourth exercise

---
+++ modules/40-lists/40-reduce/test.clj
@@ -0,0 +1,8 @@
+(ns bool-test
+  (:require [test-helper :refer [assert-solution]]
+            [index :refer [max-delta]]))
+
+(assert-solution
+ [['() '()] ['(-5) '(-15)] ['(0) '(42)] ['(10 -15 35) '(2 -12 42)]]
+ [0 10 42 8]
+ max-delta)
