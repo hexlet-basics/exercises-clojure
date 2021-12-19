de679015ff75c8919134039da294d3a2d26bc54b
solar05 <artem00298@gmail.com>
2021-12-19 03:42:29 +0300

    Add first macro exercise

--- modules/60-macros/10-intro-macros/index.clj
+++ modules/60-macros/10-intro-macros/index.clj
@@ -1,4 +1,12 @@
 (ns index)
 ;BEGIN
-(println "hello, world")
+(defn my-fn [_]
+  (println "Hello from fn!"))
+
+(defmacro my-macro [x]
+  (println "Hello from macro!")
+  x)
+
+(my-fn (println (+ 1 2)))
+(my-macro (println (+ 1 2)))
 ;END
