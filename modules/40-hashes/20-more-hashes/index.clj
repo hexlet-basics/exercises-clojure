b7ad305720bd46069d238d90e6bc78af194e6712
Соломатин Артем Николаевич <artem00298@gmail.com>
2023-10-13 00:33:48 +0300

    Change more-hashes solution

--- modules/40-hashes/20-more-hashes/index.clj
+++ modules/40-hashes/20-more-hashes/index.clj
@@ -1,5 +1,10 @@
 (ns index)
 ;BEGIN
 (defn freq [col]
-  (frequencies col))
+  (reduce (fn [acc key]
+            (if (contains? acc key)
+              (let [current-val (acc key)
+                    actual-val (inc current-val)]
+                (assoc acc key actual-val))
+              (assoc acc key 1))) {} col))
 ;END
